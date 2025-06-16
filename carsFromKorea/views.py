01906d0d2bb54fb39084b92cd4b03a0c2baf8d5b
turar <turarbekovaikol@gmail.com>
2025-06-17 03:56:28 +0600

    vehicles fixed

--- carsFromKorea/views.py
+++ carsFromKorea/views.py
@@ -11,7 +11,7 @@ from rapidfuzz import fuzz
 
 
 def load_cars():
-    db_path = os.path.join(settings.BASE_DIR, 'data', 'db', 'vehicles.db')
+    db_path = os.path.join(settings.BASE_DIR, 'data', 'db', 'cars_sql.db')
     conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
--- data/remove_duplicates.py
+++ data/remove_duplicates.py
@@ -1,15 +1,25 @@
-import sqlite3
+import aiosqlite
+import asyncio
 
-def remove_duplicates():
-    conn = sqlite3.connect('db.sqlite3')
-    conn.execute("""
-        DELETE FROM cars
-        WHERE rowid NOT IN (
-            SELECT MIN(rowid)
-            FROM cars
-            GROUP BY car_number
-        )
-    """)
-    conn.commit()
-    conn.close()
 
+# def remove_duplicates():
+#     conn = sqlite3.connect('db/cars_sql.db')
+#     conn.execute("""
+#         DELETE FROM cars
+#         WHERE rowid NOT IN (
+#             SELECT MIN(rowid)
+#             FROM cars
+#             GROUP BY car_number
+#         )
+#     """)
+#     conn.commit()
+#     conn.close()
+
+
+async def drop_table():
+    async with aiosqlite.connect('db/cars_sql.db') as conn:
+        await conn.execute("DROP TABLE IF EXISTS cars")
+        await conn.commit()
+        print("Таблица 'cars' удалена.")
+
+asyncio.run(drop_table())
